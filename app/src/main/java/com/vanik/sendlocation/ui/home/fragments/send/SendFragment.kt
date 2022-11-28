451a4b29b585f922a688b2b6c29334015ae9693c
vanikdallakyan <vanik.dallakyan@softconstruct.com>
2022-11-28 08:57:49 +0400

    some changes

--- app/src/main/java/com/vanik/sendlocation/ui/home/fragments/send/SendFragment.kt
+++ app/src/main/java/com/vanik/sendlocation/ui/home/fragments/send/SendFragment.kt
@@ -2,13 +2,12 @@ package com.vanik.sendlocation.ui.home.fragments.send
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.ContentResolver
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
@@ -17,7 +16,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.vanik.sendlocation.R
 import com.vanik.sendlocation.data.model.User
 import com.vanik.sendlocation.databinding.FragmentSendBinding
-import com.vanik.sendlocation.ui.base.BaseActivity
 import com.vanik.sendlocation.ui.home.HomeActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -25,6 +23,7 @@ class SendFragment : Fragment() {
     private val viewModel: SendViewModel by viewModel()
     private lateinit var binding: FragmentSendBinding
     private lateinit var adapter:UserAdapter
+    private val users = arrayListOf<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +37,36 @@ class SendFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_send, container, false)
         setupViews()
+        binding.userSearchView.clearFocus()
+        binding.userSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(str: String): Boolean {
+                filterList(str)
+                return true
+            }
+
+        })
         return binding.root
     }
 
-//    private fun getContacts() = context?.let { viewModel.getContacts(it) }
+    fun filterList(searchName: String){
+        val filterList = arrayListOf<User>()
+        for (user in users){
+            if(user.fullName.toLowerCase().contains(searchName.toLowerCase()) || user.phoneNumber.toLowerCase().contains(searchName.toLowerCase())){
+                    filterList.add(user)
+                (requireActivity() as HomeActivity).logChat(filterList.size.toString())
+            }
+        }
+        if(filterList.isEmpty()){
+            adapter.setFilterUsers(users)
+            (requireActivity() as HomeActivity).showMessage("no data")
+        }else{
+            adapter.setFilterUsers(filterList)
+        }
+    }
 
     private fun setupViews() {
         initializeAdapter()
@@ -57,7 +82,6 @@ class SendFragment : Fragment() {
 
     @SuppressLint("Range", "NotifyDataSetChanged")
     fun getContactList(): List<User> {
-        val list = arrayListOf<User>()
         if (ContextCompat.checkSelfPermission(
                 requireActivity(),
                 Manifest.permission.READ_CONTACTS
@@ -70,12 +94,12 @@ class SendFragment : Fragment() {
             )
         } else {
             viewModel.getContacts().observe(requireActivity()) {
-                list.addAll(it)
+                users.addAll(it)
                 adapter.notifyDataSetChanged()
                 (requireActivity() as HomeActivity).logChat(it[0].fullName)
                 (requireActivity() as HomeActivity).closeDialog()
             }
         }
-        return list
+        return users
     }
 }
--- app/src/main/java/com/vanik/sendlocation/ui/home/fragments/send/UserAdapter.kt
+++ app/src/main/java/com/vanik/sendlocation/ui/home/fragments/send/UserAdapter.kt
@@ -1,5 +1,6 @@
 package com.vanik.sendlocation.ui.home.fragments.send
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -19,6 +20,13 @@ class UserAdapter(
     }
 
     override fun getItemCount() = users.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setFilterUsers(filterUsers : List<User>){
+        users = filterUsers
+        notifyDataSetChanged()
+    }
+
     inner class UserHolder(private val binding: ItemUserBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(user: User) {
